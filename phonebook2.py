beb62fff6fe5aad9a2ee497487087e59f39b915c
lexx0002 <33981077+lexx0002@users.noreply.github.com>
2019-07-01 14:27:39 +0300

    Update phonebook2.py

--- phonebook2.py
+++ phonebook2.py
@@ -12,11 +12,11 @@ with open("phonebook_raw.csv", encoding='utf8') as f:
             count += 1
         else:
             line_raw = line[0] + ' ' + line[1] +  ' ' + line[2] +  ' ' + line[3] +  ' ' + line[4] +  ' ' + line[5] +  ' ' + line[6]
-            print(line_raw)
             pattern = '[А-Я][а-я]+([,]|\s)[А-я][а-я]+(\s|)([А-Я][а-я]+|)[,]*[^,]?[,]([^,]+|[,])[,]?(\+7|8)?\s?[(]?\d{0,3}[)]?\s?[-]?\d{0,3}[-]?\d{0,2}[-]?\d{0,2}[,].+'
             mask = re.findall(line_raw, pattern)
-            
+            text = re.sub(pattern, r"\1,\3,\5,\6,\7,+7(\11)\13-\15\16 \17 \18", line)
             print(mask)
+            print(text)
 
 
 
